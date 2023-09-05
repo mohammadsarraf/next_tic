280384753fa0881a9df8eb17d7418e0a03356a8c
Lilglu4e <mohammadsarrafmstr2000@gmail.com>
2023-09-04 23:12:59 -0400

    css fix

--- src/app/Components/Options.tsx
+++ src/app/Components/Options.tsx
@@ -19,37 +19,18 @@ const Options: React.FC<OptionsProps> = (props) => {
 		setIsClicked(!isClicked);
 	};
 
+	const width = screen.width
 	return (
-		<>				<div className={`flex flex-col relative  max-w-min`}>
-					<button className={`bg-violet-800 text-white font-bold w-20 h-20 text-4xl text-center rounded-lg m-5 mt-2 ml-2  cursor-pointer z-10`} onClick={handleClick}
-					>
-						⚙
-					</button>
-					<button className={`bg-violet-800 text-white font-bold w-20 h-20 text-xl text-center rounded-lg m-5 mt-2 ml-2  cursor-pointer ${toggleAnimationClassSecondChild}`} onClick={props.joinGame}>Join Game</button>
-					<button className={`bg-violet-800 text-white font-bold w-20 h-20 text-xl text-center rounded-lg  m-5 mt-2 ml-2 cursor-pointer ${toggleAnimationClassThirdChild}`} onClick={props.resetGame}>Reset Game</button>
-					<button className={`bg-violet-800 text-white font-bold w-20 h-20 text-xl text-center rounded-lg m-5 mt-2 ml-2  cursor-pointer ${toggleAnimationClassForthChild}`}>HTP</button>
-				</div>
-			{/* {isClicked ? (		
-				<div className={`flex flex-col relative  max-w-min ${isClicked ? ("bg-red-700"):("bg-blue-600")}`} onClick={handleClick}>
-					<button className={`bg-violet-800 text-white font-bold w-20 h-20 text-4xl text-center rounded-lg m-5 mt-2 ml-2  cursor-pointer z-10`}
-					>
-						⚙
-					</button>
-					<button className={`bg-violet-800 text-white font-bold w-20 h-20 text-xl text-center rounded-lg m-5 mt-2 ml-2  cursor-pointer ${toggleAnimationClassSecondChild}`} onClick={props.joinGame}>Join Game</button>
-					<button className={`bg-violet-800 text-white font-bold w-20 h-20 text-xl text-center rounded-lg  m-5 mt-2 ml-2 cursor-pointer ${toggleAnimationClassThirdChild}`} onClick={props.resetGame}>Reset Game</button>
-					<button className={`bg-violet-800 text-white font-bold w-20 h-20 text-xl text-center rounded-lg m-5 mt-2 ml-2  cursor-pointer ${toggleAnimationClassForthChild}`}>HTP</button>
-				</div>
-				):(
-				<div className={`flex flex-col relative  max-w-min ${isClicked ? ("max-h-min"):("max-h-min")}`} onClick={handleClick}>
-					<button className={`bg-violet-800 text-white font-bold w-20 h-20 text-4xl text-center rounded-lg m-5 mt-2 ml-2  cursor-pointer z-10`}
-					>
-						⚙
-					</button>
-					<button className={`bg-violet-800 text-white font-bold w-20 h-20 text-xl text-center rounded-lg m-5 mt-2 ml-2  cursor-pointer ${toggleAnimationClassSecondChild}`} onClick={props.joinGame}>Join Game</button>
-					<button className={`bg-violet-800 text-white font-bold w-20 h-20 text-xl text-center rounded-lg  m-5 mt-2 ml-2 cursor-pointer ${toggleAnimationClassThirdChild}`} onClick={props.resetGame}>Reset Game</button>
-					<button className={`bg-violet-800 text-white font-bold w-20 h-20 text-xl text-center rounded-lg m-5 mt-2 ml-2  cursor-pointer ${toggleAnimationClassForthChild}`}>HTP</button>
-				</div>
-				)} */}
+		<>
+			<div className={`flex flex-col relative max-w-min `}>
+				<button className={`bg-violet-800 text-white font-bold ${(width < 500) ? (' w-12 h-12') : ('w-20 h-20')} h-20 text-4xl text-center rounded-lg m-5 mt-2 ml-2  cursor-pointer z-10`} onClick={handleClick}
+				>
+					⚙
+				</button>
+				<button className={`bg-violet-800 text-white font-bold ${(width < 500) ? (' w-12 h-12') : ('w-20 h-20')} h-20 text-xl text-center rounded-lg  m-5 mt-2 ml-2  cursor-pointer ${toggleAnimationClassSecondChild}`} onClick={props.joinGame}>Join Game</button>
+				<button className={`bg-violet-800 text-white font-bold ${(width < 500) ? (' w-12 h-12') : ('w-20 h-20')} h-20 text-xl text-center rounded-lg  m-5 mt-2 ml-2 cursor-pointer ${toggleAnimationClassThirdChild}`} onClick={props.resetGame}>Reset Game</button>
+				<button className={`bg-violet-800 text-white font-bold ${(width < 500) ? (' w-12 h-12') : ('w-20 h-20')} h-20 text-xl text-center rounded-lg  m-5 mt-2 ml-2  cursor-pointer ${toggleAnimationClassForthChild}`}>HTP</button>
+			</div>
 		</>
 	);
 }
